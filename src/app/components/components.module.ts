fbea76b7c173532b17bd528b873e2117dc61a679
Gabriel Mascarenhas <devgabrielprog@gmail.com>
2025-12-12 09:14:14 -0300

    criado modulo do angular material e componente

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from "@angular/core";
+import { AngularMaterialModule } from "../material/angularmaterial.module";
 
 @NgModule({
     declarations:[],
-    imports:[],
-    exports:[]
+    imports:[AngularMaterialModule],
+    exports:[AngularMaterialModule]
 })
 export class ComponentsModule {
 
