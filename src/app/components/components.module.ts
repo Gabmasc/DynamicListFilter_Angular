7643cf16dca18774c29dc19dad3364defb6acd56
Gabriel Mascarenhas <devgabrielprog@gmail.com>
2025-12-12 22:33:43 -0300

    component user-list criado

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -2,17 +2,20 @@ import { NgModule } from "@angular/core";
 import { AngularMaterialModule } from "../material/angularmaterial.module";
 import { FilterComponent } from './filter/filter.component';
 import { UserDetailComponent } from './user-detail/user-detail.component';
+import { UserListComponent } from './user-list/user-list.component';
 
 @NgModule({
     declarations:[
     FilterComponent,
-    UserDetailComponent
+    UserDetailComponent,
+    UserListComponent
   ],
     imports:[AngularMaterialModule],
     exports:[
       AngularMaterialModule,
       FilterComponent,
-      
+      UserDetailComponent,    
+      UserListComponent
     ]
 })
 export class ComponentsModule {
