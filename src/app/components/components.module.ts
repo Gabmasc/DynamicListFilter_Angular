1ea816a2199296241d53fac2564e6115f289d8e7
Gabriel Mascarenhas <devgabrielprog@gmail.com>
2025-12-12 22:25:20 -0300

    componente de filtro criado

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from "@angular/core";
 import { AngularMaterialModule } from "../material/angularmaterial.module";
+import { FilterComponent } from './filter/filter.component';
 
 @NgModule({
-    declarations:[],
+    declarations:[
+    FilterComponent
+  ],
     imports:[AngularMaterialModule],
     exports:[AngularMaterialModule]
 })
