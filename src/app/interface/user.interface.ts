fa29c41f4ef0fdb165bcce7059d2a1aa0e038aed
Gabriel Mascarenhas <devgabrielprog@gmail.com>
2025-12-15 20:24:32 -0300

    aplicado a inteface que define o objeto endereco dentro do objeto User

--- src/app/interface/user.interface.ts
+++ src/app/interface/user.interface.ts
@@ -1,3 +1,4 @@
+import { IEndereco } from "./endereco.interface";
 import { IStatus } from "./status.interface";
 
 export interface IUser{
@@ -6,8 +7,9 @@ export interface IUser{
     senha: string;
     idade: number;
     telefone: string;
+    endereco: IEndereco;
     ativo: boolean;
     funcao: string;
-    datacadastro: string
-    status: IStatus
+    datacadastro: string;
+    status: IStatus;
 }
