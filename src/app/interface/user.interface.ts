adaf4f61521d91bae14dc71ab936bd88f3338332
Gabriel Mascarenhas <devgabrielprog@gmail.com>
2025-12-15 20:17:46 -0300

    Aplicado a interface que define o objeto USER

---
+++ src/app/interface/user.interface.ts
@@ -0,0 +1,13 @@
+export interface IUser{
+    nome: string;
+    email: string;
+    senha: string;
+    idade: number;
+    telefone: string;
+    ativo: boolean;
+    funcao: string;
+    datacadastro: string
+
+
+
+}
