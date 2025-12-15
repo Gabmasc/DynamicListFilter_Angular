bde96242d30668611ba966e572ccfa4eb8cbbcf1
Gabriel Mascarenhas <devgabrielprog@gmail.com>
2025-12-15 20:11:22 -0300

    Implementado metodo responsavel por enviar mensagens para a aplicacao pai

--- src/app/components/user-list/user-list.component.ts
+++ src/app/components/user-list/user-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-user-list',
@@ -8,4 +8,10 @@ import { Component } from '@angular/core';
 export class UserListComponent {
 
   displayedColumns : string [] = ['Nome', 'Data', 'Status']
+
+  @Output('userSelected') userSelectedEmitt = new EventEmitter<>()
+
+  onUserSelected(){
+
+  }
 }
