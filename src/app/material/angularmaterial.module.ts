62de5b50f6a2e88c6493232ebbe4080017ea438c
Gabriel Mascarenhas <devgabrielprog@gmail.com>
2025-12-15 21:02:32 -0300

    Implementados componentes input e formfield do angular material

--- src/app/material/angularmaterial.module.ts
+++ src/app/material/angularmaterial.module.ts
@@ -1,20 +1,30 @@
 import { NgModule } from "@angular/core";
 import { MatListModule } from '@angular/material/list';
+import { MatInputModule } from '@angular/material/input';
 import { MatDividerModule } from '@angular/material/divider';
-import {MatTableModule} from '@angular/material/table';
+import { MatTableModule } from '@angular/material/table';
+import { MatFormFieldModule } from '@angular/material/form-field';
+
+
 
 
 @NgModule({
     imports:[
         MatListModule,
+        MatInputModule,
+        MatTableModule,
         MatDividerModule,
-        MatTableModule
+        MatFormFieldModule,
+        
         
     ],
     exports:[
         MatListModule,
+        MatInputModule,
+        MatTableModule,
         MatDividerModule,
-        MatTableModule
+        MatFormFieldModule
+        
     ]
 })
 export class AngularMaterialModule {}
