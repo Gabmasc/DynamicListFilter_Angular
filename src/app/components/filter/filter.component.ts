b0ec635481a0af63b18bcf0b6043b30b4874218f
Gabriel Mascarenhas <devgabrielprog@gmail.com>
2025-12-15 21:25:45 -0300

    aplicado estilizacao e logica de listagem conforme o tipo de status do componente

--- src/app/components/filter/filter.component.ts
+++ src/app/components/filter/filter.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class FilterComponent {
 
+  statusList = [
+    {description: 'Ativo', value: true},
+    {description: 'Inativo', value: false}
+  ];
+
 }
