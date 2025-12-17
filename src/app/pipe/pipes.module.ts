a09b65faad362151ddbfe04fdeacb48c86bf0a7c
Gabriel Mascarenhas <devgabrielprog@gmail.com>
2025-12-17 09:04:07 -0300

    feat(pipe): cria pipe de status com validacao e aplica no user-detail e user-list Evita erros quando os dados de status nao estao definidos e formata caso o dado de status seja true ou false padroniza a exibicao no componente user-detail e user-list

--- src/app/pipe/pipes.module.ts
+++ src/app/pipe/pipes.module.ts
@@ -1,15 +1,18 @@
 import { NgModule } from "@angular/core";
 import { EnderecoPipe } from './endereco.pipe';
 import { PhonePipe } from './phone.pipe';
+import { StatusPipe } from './status.pipe';
 
 @NgModule({
     declarations: [
     EnderecoPipe,
-    PhonePipe
+    PhonePipe,
+    StatusPipe
   ],
     exports: [
         EnderecoPipe,
-        PhonePipe
+        PhonePipe,
+        StatusPipe
     ]
 })
 export class PipesModule{}
