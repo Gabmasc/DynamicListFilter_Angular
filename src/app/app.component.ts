aef8f3b74500bd3789e54a69aa63b21ab59bc373
Gabriel Mascarenhas <devgabrielprog@gmail.com>
2025-12-22 09:36:07 -0300

    feat(app): Cria metodo e filtra lista pela data de cadastro Filtra pelo intervalo entre data incial e data final

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { IUser } from './interface/user.interface';
 import { UserList } from './DATA/users-list';
 import { IFilterOptions } from './interface/filter-options/filter-options.interface';
+import { isWithinInterval } from 'date-fns';
 
 @Component({
   selector: 'app-root',
@@ -34,7 +35,26 @@ onFilter(filterOptions: IFilterOptions){
 }
 
 filterUsersList(filterOptions: IFilterOptions, userList: IUser[]): IUser[] {
-    ;
+  let filteredList: IUser[] = [];
+  
+  filteredList = this.filterUsersListByDate(filterOptions.startDate, filterOptions.endDate, filteredList);
+  
+
+  return filteredList;
+  }
+
+  filterUsersListByDate(startDate: Date | undefined, endDate: Date | undefined, filteredList: IUser[]): IUser[] {
+    const DATES_NOT_SELECTED = startDate === undefined || endDate === undefined;
+    if(DATES_NOT_SELECTED){
+      return filteredList;
+    }
+
+    const userListFilteredList = filteredList.filter((user) => isWithinInterval(new Date(user.datacadastro), {
+      start: startDate,
+      end: endDate
+    }));
+
+    return userListFilteredList;
   }
 
 }
