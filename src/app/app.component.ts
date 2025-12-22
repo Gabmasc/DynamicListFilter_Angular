b3354c0447235615374749463cee8a584f563fef
Gabriel Mascarenhas <devgabrielprog@gmail.com>
2025-12-22 09:09:21 -0300

    feat(app): Implementa metodo de filtro e mitido pelo elemento filho. Cria metodo que filtra lista de usuario.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { IUser } from './interface/user.interface';
 import { UserList } from './DATA/users-list';
+import { IFilterOptions } from './interface/filter-options/filter-options.interface';
 
 @Component({
   selector: 'app-root',
@@ -13,13 +14,27 @@ userSelected: IUser = {} as IUser;
 userList: IUser [] = [];
 userListFiltered: IUser [] = [];
 
-
 ngOnInit() {
   setTimeout(() => {
     this.userList = UserList;
     this.userListFiltered = this.userList;
-  })
+  }, 2000)
+};
+
+onUserSelected(user: IUser){
+  this.userSelected = user;
+
+  this.showUserDetails = true;
 }
 
+onFilter(filterOptions: IFilterOptions){
+  console.log(filterOptions);
+
+  this.userListFiltered = this.filterUsersList(filterOptions, this.userList);
+}
+
+filterUsersList(filterOptions: IFilterOptions, userList: IUser[]): IUser[] {
+    ;
+  }
 
 }
