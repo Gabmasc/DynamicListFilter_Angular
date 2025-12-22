4252e1eeadc2de258b8b8d51660b118ee13eb09d
Gabriel Mascarenhas <devgabrielprog@gmail.com>
2025-12-22 08:18:38 -0300

    feat(app): Implementa interface para simular chamada http com delay de 2 segundos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { IUser } from './interface/user.interface';
+import { UserList } from './DATA/users-list';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
 showUserDetails: boolean = false;
 userSelected: IUser = {} as IUser;
 userList: IUser [] = [];
 userListFiltered: IUser [] = [];
+
+
+ngOnInit() {
+  setTimeout(() => {
+    this.userList = UserList;
+    this.userListFiltered = this.userList;
+  })
+}
+
+
 }
